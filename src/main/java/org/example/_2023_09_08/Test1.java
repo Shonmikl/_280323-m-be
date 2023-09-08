fdc51c7a67b285127fa93e37a4f330fe673a791f
shonmikl <mikhailegorov676@gmail.com>
2023-09-08 10:09:22 +0200

    Stream API Example

---
+++ src/main/java/org/example/_2023_09_08/Test1.java
@@ -0,0 +1,26 @@
+package org.example._2023_09_08;
+
+import java.util.Arrays;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int[] arr = {24, 58, 97, 65, 63, 102, 5, 41};
+
+//        Arrays.stream(arr)
+//                .filter(el -> el % 2 == 0)
+//                .sorted() //todo
+//                .forEach(System.out::println);
+
+        int[] s = Arrays.stream(arr)
+                .map(el -> {
+                    if (el % 2 == 0) {
+                        System.out.println(el - 10);
+                    }
+                    return el * 2;
+                })
+//                .peek(System.out::println);
+                .toArray();
+
+//        System.out.println(Arrays.toString(s));
+    }
+}
